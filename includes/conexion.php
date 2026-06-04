47cc20adcdcc4e209557f993c9f806a375e45e79
FicTy17 <mate.scatolonet32@gmail.com>
2026-06-04 14:26:49 -0300

    Add files via upload

---
+++ includes/conexion.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$clave = "";
+$base_datos = "proyectofutbol";
+
+$conexion = new mysqli($host, $usuario, $clave, $base_datos);
+
+if ($conexion->connect_error) {
+    die("❌ Error de conexión: " . $conexion->connect_error);
+}
+
+$conexion->set_charset("utf8mb4");
+?>
